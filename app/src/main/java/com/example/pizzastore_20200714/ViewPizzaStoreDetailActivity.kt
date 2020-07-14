948719b0ed07ea9bb4031a1ea4e60bc863b73a8a
이예원 <gokaiko@naver.com>
2020-07-14 22:21:21 +0900

    pizzaStoreDetail.xml made

---
+++ app/src/main/java/com/example/pizzastore_20200714/ViewPizzaStoreDetailActivity.kt
@@ -0,0 +1,19 @@
+package com.example.pizzastore_20200714
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ViewPizzaStoreDetailActivity : BaseActivity() {
+    override fun setupEvents() {
+        TODO("Not yet implemented")
+    }
+
+    override fun setValues() {
+        TODO("Not yet implemented")
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_view_pizza_store_detail)
+    }
+}
