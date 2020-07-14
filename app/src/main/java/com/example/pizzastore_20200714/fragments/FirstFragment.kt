0c5b1b0cb8ceb0034f275e49ae5ba8b68eea45ee
이예원 <gokaiko@naver.com>
2020-07-14 20:27:12 +0900

    added TapLayout in activity_main.xml

--- app/src/main/java/com/example/pizzastore_20200714/fragments/FirstFragment.kt
+++ app/src/main/java/com/example/pizzastore_20200714/fragments/FirstFragment.kt
@@ -1,13 +1,24 @@
 package com.example.pizzastore_20200714.fragments
 
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.pizzastore_20200714.R
 
 class FirstFragment : Fragment() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.activity_first_fragment, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
 
     }
 }
